2c7f53de74dd88e5c478df6df13b2e859fce2589
Sammie <sroland@engr-134-07.eastern.ewu.edu>
2015-03-09 13:35:14 -0700

    final

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ $('#sky').jQlouds({
 $('#sky').jQlouds({
   maxWidth: 300, // max image's width
   minWidth: 200, // min image's width
-  maxHeight: 48, // amx image's height
+
 });
 
 $('#sky').jQlouds({
