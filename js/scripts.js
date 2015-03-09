e3b64138ad427f0a604b8ce2b8135acb18b365bf
Sammie <sroland@engr-134-07.eastern.ewu.edu>
2015-03-09 13:17:13 -0700

    width/wind

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+
+
+$('#sky').jQlouds({
+  maxWidth: 300, // max image's width
+  minWidth: 200, // min image's width
+  maxHeight: 48, // amx image's height
+});
+
+$('#sky').jQlouds({
+  wind: true
+});
